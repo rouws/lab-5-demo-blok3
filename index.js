b1355787153b1512fc5ad56ffcacad80d3734270
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2021-02-09 12:58:48 +0100

    add route for movies

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+app.get('/movies', (req, res) => {
+    res.send('A list of movies')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
